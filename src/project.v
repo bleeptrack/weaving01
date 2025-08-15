c3aaaa70f78cb8ad550f893b63bedde974034fc5
bleeptrack <mail@bleeptrack.de>
2025-08-15 10:45:26 +0200

    init

--- src/project.v
+++ src/project.v
@@ -5,6 +5,10 @@
 
 `default_nettype none
 
+(* blackbox *) (* keep *)
+module my_logo ();
+endmodule
+
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -14,6 +18,10 @@ module tt_um_example (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
+    
+    (* keep *)
+    my_logo logo();
+
 );
 
   // All output pins must be assigned. If not used, assign to 0.
