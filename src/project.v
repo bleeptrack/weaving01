055401e84bd49cd7327dde07a07a1c8c9446095e
bleeptrack <mail@bleeptrack.de>
2025-08-15 11:02:28 +0200

    fix module init

--- src/project.v
+++ src/project.v
@@ -18,11 +18,10 @@ module tt_um_example (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
-    
-    (* keep *)
-    my_logo logo();
-
 );
+
+  (* keep *)
+  my_logo logo();
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
