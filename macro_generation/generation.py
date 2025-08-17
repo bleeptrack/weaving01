7a64e72fcb9128b24fe673cea2e314af0b0d9829
bleeptrack <mail@bleeptrack.de>
2025-08-17 18:15:19 +0200

    revert

--- macro_generation/generation.py
+++ macro_generation/generation.py
@@ -29,19 +29,26 @@ cell.add(pr_boundary)
 def write_lef_file(filename, cell_name, cell_bounds, pins):
     """Write a LEF file for the cell"""
     with open(filename, 'w') as f:
-        f.write("VERSION 5.7 ;\n")
-        f.write("  NOWIREEXTENSIONATPIN ON ;\n")
-        f.write("  DIVIDERCHAR \"/\" ;\n")
-        f.write("  BUSBITCHARS \"[]\" ;\n")
-        f.write("MACRO {}\n".format(cell_name))
-        f.write("  CLASS BLOCK ;\n")
-        f.write("  FOREIGN {} ;\n".format(cell_name))
-        f.write("  SIZE {:.3f} BY {:.3f} ;\n".format(cell_bounds[2] - cell_bounds[0], cell_bounds[3] - cell_bounds[1]))
+        f.write("# LEF file generated for {}\n".format(cell_name))
+        f.write("VERSION 5.8 ;\n")
+        f.write("NAMESCASESENSITIVE ON ;\n")
+        f.write("DIVIDERCHAR \"/\" ;\n")
+        f.write("BUSBITCHARS \"[]\" ;\n")
+        f.write("UNITS\n")
+        f.write("   DATABASE MICRONS 1000 ;\n")
+        f.write("END UNITS\n\n")
         
-        # No pins - minimal blackbox macro
+        # Define the cell
+        f.write("MACRO {}\n".format(cell_name))
+        f.write("   CLASS BLOCK ;\n")
+        f.write("   FOREIGN {} 0 0 ;\n".format(cell_name))
+        f.write("   SIZE {:.3f} BY {:.3f} ;\n".format(cell_bounds[2] - cell_bounds[0], cell_bounds[3] - cell_bounds[1]))
+        f.write("   SYMMETRY X Y ;\n")
+        
+        # No pins - pure blackbox module
+        # No OBS section needed for pure artwork on TopMetal1
         
         f.write("END {}\n".format(cell_name))
-        f.write("END LIBRARY\n")
 
 # Calculate cell bounds (back to original size)
 cell_width = 32  # 32 microns
