91a1c4649e1ad3df9f00f0f6c7ef9d2ac8b0a4d5
bleeptrack <mail@bleeptrack.de>
2025-08-17 18:31:48 +0200

    fixing verilog

--- src/project.v
+++ src/project.v
@@ -25,10 +25,9 @@ module tt_um_example (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = 8'b0;  // Set all bits to 0
+  assign uio_oe  = 8'b0;  // Set all bits to 0 (all pins as inputs)
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  // No unused inputs warning needed since we're using all inputs
 
 endmodule
