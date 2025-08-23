f2bb9e8d9f8c5fea7d9f457c999cc2203f7b5105
bleeptrack <mail@bleeptrack.de>
2025-08-23 13:07:38 +0200

    placement

--- macro_generation/generation.py
+++ macro_generation/generation.py
@@ -9,8 +9,8 @@ length = 8  # Reduced from 8
 avg_width = 4  # Reduced from 3
 gap = 1  # Reduced from 1
 
-sizeX = 13
-sizeY = 13
+sizeX = 15
+sizeY = 15
 
 structure = [[random.randint(0, 1) for _ in range(sizeY)] for _ in range(sizeX)]
 
