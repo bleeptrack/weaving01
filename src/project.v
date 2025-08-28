19dc445bf86845c6b57fabca9bb4088158d1400c
bleeptrack <mail@bleeptrack.de>
2025-08-28 19:53:35 +0200

    adding info

--- src/project.v
+++ src/project.v
@@ -9,7 +9,7 @@
 module my_logo ();
 endmodule
 
-module tt_um_example (
+module tt_blptrk_weaving01 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
